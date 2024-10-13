7012dec6e8b4db04bf1e7b47f402a95a74d4729e
arpit <arpitsa@ntnu.no>
2024-10-13 19:23:51 +0200

    corrected my spelling of my code and added a constructor in the amount class

--- src/main/java/edu/ntnu/idi/bidata/Amount.java
+++ src/main/java/edu/ntnu/idi/bidata/Amount.java
@@ -1,7 +1,36 @@
 package edu.ntnu.idi.bidata;
 
 public class Amount {
-    private Float amountKg;
-    private Float amountLiters;
-    private Integer amoutPieces;
+    private Float kilogram;
+    private Float liters;
+    private Integer pieces;
+
+    public Amount(Float kilogram, Float liters, Integer pieces){
+        setKilogram(kilogram);
+        setLiters(liters);
+        setPieces(pieces);
+    }
+    public Float getKilogram() {
+        return kilogram;
+    }
+
+    public void setKilogram(Float kilogram) {
+        this.kilogram = kilogram;
+    }
+
+    public Float getLiters() {
+        return liters;
+    }
+
+    public void setLiters(Float liters) {
+        this.liters = liters;
+    }
+
+    public Integer getPieces() {
+        return pieces;
+    }
+
+    public void setPieces(Integer pieces) {
+        this.pieces = pieces;
+    }
 }
--- src/main/java/edu/ntnu/idi/bidata/Food.java
+++ src/main/java/edu/ntnu/idi/bidata/Food.java
@@ -7,7 +7,43 @@ import java.time.LocalDate;
  */
 public class Food {
     private String nameOfFood;
-    private Integer PriceOfFood;
-    private LocalDate experiationOfFood;
+    private Integer priceOfFood;
+    private LocalDate expirationDate;
     private Amount amountOfFood;
+
+
+    public String getNameOfFood() {
+        return nameOfFood;
+    }
+
+    public void setNameOfFood(String nameOfFood) {
+        this.nameOfFood = nameOfFood;
+    }
+
+    public Integer getPriceOfFood() {
+        return priceOfFood;
+    }
+
+    public void setPriceOfFood(Integer priceOfFood) {
+        this.priceOfFood = priceOfFood;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public Amount getAmountOfFood() {
+        return amountOfFood;
+    }
+
+    public void setAmountOfFood(Amount amountOfFood) {
+        this.amountOfFood = amountOfFood;
+
+    }
+
+
 }
