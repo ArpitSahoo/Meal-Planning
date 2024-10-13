d3ebe18b81d845e04b8b61a42f1a90e7388c08b9
arpit <arpitsa@ntnu.no>
2024-10-13 19:30:22 +0200

    corrected my spelling of my code and added a constructor in the amount class. Everything got deleted by git, so i had to start all over again. generated getter and setters.

--- src/main/java/edu/ntnu/idi/bidata/Food.java
+++ src/main/java/edu/ntnu/idi/bidata/Food.java
@@ -7,5 +7,46 @@ import java.time.LocalDate;
  */
 public class Food {
     private String nameOfFood;
-    private Amount amountOfFood;
+    private Amount amount;
+    private LocalDate expirationDate;
+    private float price;
+
+    public Food(String nameOfFood, Amount amount, LocalDate expirationDate, float price) {
+        this.nameOfFood = nameOfFood;
+        this.amount = amount;
+        this.expirationDate = expirationDate;
+        this.price = price;
+    }
+
+    public String getNameOfFood() {
+        return nameOfFood;
+    }
+
+    public void setNameOfFood(String nameOfFood) {
+        this.nameOfFood = nameOfFood;
+    }
+
+    public Amount getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Amount amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
--- src/main/java/edu/ntnu/idi/bidata/Main.java
+++ src/main/java/edu/ntnu/idi/bidata/Main.java
@@ -3,6 +3,10 @@ package edu.ntnu.idi.bidata;
 /**
  * The main starting point of your application. Let this class create the
  * instance of your main-class that starts your application.
+ *
+ * @author Arpit Sahoo
+ * @version 0.0.1
  */
 public class Main {
+
 }
