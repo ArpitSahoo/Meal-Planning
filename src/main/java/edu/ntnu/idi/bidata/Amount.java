c32d2f0a03d1f191827e02dff70aba63f4f24675
arpit <arpitsa@ntnu.no>
2024-10-14 12:57:37 +0200

    Minor testing changes

--- src/main/java/edu/ntnu/idi/bidata/Amount.java
+++ src/main/java/edu/ntnu/idi/bidata/Amount.java
@@ -1,19 +1,56 @@
 package edu.ntnu.idi.bidata;
 
 public class Amount {
+    private Float amount;
+    private String units;
     private Float kilogram;
+    private Float gram;
     private Float liters;
     private Integer pieces;
 
-    public Amount(Float kilogram, Float liters, Integer pieces){
+    public Amount(Float amount, String units){
+        setAmount(amount);
+        setUnits(units);
+        /*
+        setKilogram(kilogram);
+        setGram(gram);
+        setLiters(liters);
+        setPieces(pieces);
+         */
 
     }
+
+    public Float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Float amount) {
+        this.amount = amount;
+    }
+
+    public String getUnits() {
+        return units;
+    }
+
+    public void setUnits(String units) {
+        this.units = units;
+    }
+
+    /*
     public Float getKilogram() {
         return kilogram;
     }
 
     public void setKilogram(Float kilogram) {
         this.kilogram = kilogram;
+    }
+
+    public Float getGram() {
+        return gram;
+    }
+
+    public void setGram(Float gram) {
+        this.gram = gram;
     }
 
     public Float getLiters() {
@@ -31,5 +68,7 @@ public class Amount {
     public void setPieces(Integer pieces) {
         this.pieces = pieces;
     }
+     */
+
 }
 
