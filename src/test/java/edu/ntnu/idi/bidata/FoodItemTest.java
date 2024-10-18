4a110053b116e109d45b2ed89e66ebd594791367
arpit <arpitsa@ntnu.no>
2024-10-18 11:58:30 +0200

    Tried to make a test class, but failed.

---
+++ src/test/java/edu/ntnu/idi/bidata/FoodItemTest.java
@@ -0,0 +1,44 @@
+package edu.ntnu.idi.bidata;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+class FoodItemTest {
+    static FoodItem foodItemTest;
+
+    @org.junit.jupiter.api.Test
+    void getNameOfFood() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setNameOfFood() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAmount() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setAmount() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getUnits() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setUnits() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getExpirationDate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setExpirationDate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPrice() {
+    }
+
+}
