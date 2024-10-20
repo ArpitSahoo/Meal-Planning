a28dfff9a70d0012c75c26641c297e7ceea237a7
arpit <arpitsa@ntnu.no>
2024-10-20 17:55:26 +0200

    Made an addFoodItem function and try/catch

--- src/main/java/edu/ntnu/idi/bidata/FridgeApp.java
+++ src/main/java/edu/ntnu/idi/bidata/FridgeApp.java
@@ -1,5 +1,7 @@
 package edu.ntnu.idi.bidata;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -16,8 +18,82 @@ public class FridgeApp {
     public ArrayList<FoodItem> fridge = new ArrayList<>();
     public Scanner scanner;
 
+    public void run(){
+        boolean running = true;
 
-    public static void main(String[] args){
+        while (running){
+            System.out.println("\n--- Fridge Management ---");
+            System.out.println("1. Add food");
+            System.out.println("2. Remove food");
+            System.out.println("3. Display fridge contents");
+            System.out.println("4. Check for expired food");
+            System.out.println("5. Exit");
+            System.out.println("Choose a following number: ");
+
+
+            int chosen = scanner.nextInt();
+            scanner.nextLine();
+
+            switch(chosen){
+                case 1:
+                    addFoodItem();
+                    break;
+            }
+        }
+
+
+
+    }
+
+    private void addFoodItem(){
+        try{
+            System.out.print("Hva is the name of the food: ");
+            String nameOfFood = scanner.nextLine();
+
+            System.out.println("What is the quantity: ");
+            Float amount = scanner.nextFloat();
+
+            System.out.println("Choose unit:");
+            System.out.println("1. kg");
+            System.out.println("2. liter");
+            System.out.println("3. gram");
+            System.out.println("4. pieces");
+
+            int unitChosen = scanner.nextInt();
+
+            String units;
+            switch (unitChosen){
+                case 1:
+                    units = "kg";
+                    break;
+                case 2:
+                    units = "liter";
+                    break;
+                case 3:
+                    units = "gram";
+                    break;
+                case 4:
+                    units = "stk";
+                    break;
+                default:
+                    System.out.println("Invalid unit choice. Food item not added.");
+                    return;
+            }
+
+            System.out.println("What is the price of the food");
+            Double price = scanner.nextDouble();
+
+
+            System.out.print("Enter expiration date (yyyy-MM-dd): ");
+            String expiration = scanner.next();
+            LocalDate expirationDate = LocalDate.parse(expiration, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+            FoodItem food = new FoodItem(nameOfFood, amount, units, price, expirationDate);
+            fridge.add(food);
+        }
+        catch(IllegalArgumentException illegalArgument){
+            System.out.println(illegalArgument.getMessage());
+        }
 
     }
 }
