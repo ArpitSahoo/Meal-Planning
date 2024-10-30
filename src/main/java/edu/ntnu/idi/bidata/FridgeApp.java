fee59c04195d490d09c07f6286499453a426d445
arpit <arpitsa@ntnu.no>
2024-10-30 14:47:41 +0100

    Fixed my documentation, Made the ArrayList more abstract. Changed some of the print to make it fit the google checks.

--- src/main/java/edu/ntnu/idi/bidata/FridgeApp.java
+++ src/main/java/edu/ntnu/idi/bidata/FridgeApp.java
@@ -5,7 +5,9 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
+
 import static java.lang.System.*;
 
 /**
@@ -18,214 +20,211 @@ import static java.lang.System.*;
 
 
 public class FridgeApp {
-    private final ArrayList<FoodItem> fridge;
-    private final Scanner scanner;
+  private final List<FoodItem> fridge;
+  private final Scanner scanner;
 
-    /**
-     * A constructor that creat a new fridge arraylist.
-     * Creates Scanner
-     */
-    private FridgeApp(){
-        fridge = new ArrayList<>();
-        scanner = new Scanner(in);
+  /**
+   * A constructor that creat a new fridge arraylist.
+   * Creates Scanner.
+   */
+  private FridgeApp() {
+    fridge = new ArrayList<>();
+    scanner = new Scanner(in);
+  }
+
+  /**
+   * A function that keeps the program running.
+   * Print's out different commands.
+   * Switch case to choose.
+   */
+  private void run() {
+    boolean running = true;
+    UserinterFase uiFridgeApp = new UserinterFase();
+    uiFridgeApp.start();
+    while (running) {
+      uiFridgeApp.choiceScreen();
+      String chosen = scanner.nextLine();
+
+      switch (chosen) {
+        case "1":
+          addFoodItem();
+          break;
+        case "2":
+          removeFoodItem();
+          break;
+        case "3":
+          takeOutItem();
+          break;
+        case "4":
+          displayFridge();
+          break;
+        case "5":
+          checkExpiredFood();
+          break;
+        case "6":
+          running = false;
+          out.println("System ending...");
+          out.println("Goodbye....");
+          break;
+        default:
+          out.println("Invalid choice. Please try again.");
+      }
+    }
+  }
+
+  /**
+   * Function to add food.
+   * Switch case to choose units.
+   * IllegalArgumentException argument to catch illegalArgument.
+   */
+  private void addFoodItem() {
+    try {
+      out.print("What is the name of the food: ");
+      String nameOfFood = scanner.nextLine();
+
+      out.println("What is the quantity: ");
+      Float amount = scanner.nextFloat();
+      scanner.nextLine();
+
+      UserinterFase uiFridgeApp = new UserinterFase();
+      uiFridgeApp.choiceOfUnits();
+
+      out.println("Which unit is the food?: ");
+      int unitChosen = scanner.nextInt();
+      scanner.nextLine();
+
+
+      String units;
+      switch (unitChosen) {
+        case 1:
+          units = "kg";
+          break;
+        case 2:
+          units = "liter";
+          break;
+        case 3:
+          units = "gram";
+          break;
+        case 4:
+          units = "stk";
+          break;
+        default:
+          out.println("Invalid unit choice. Food item not added.");
+          return;
+      }
+
+
+      out.println("What is the price per unit of the food (Kilo/KR):");
+      Double price = scanner.nextDouble();
+      scanner.nextLine();
+
+      out.print("Enter expiration date (yyyy-MM-dd): ");
+      String expiration = scanner.nextLine();
+      LocalDate expirationDate = LocalDate.parse(expiration, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+
+      FoodItem food = new FoodItem(nameOfFood, amount, units, price, expirationDate);
+      fridge.add(food);
+      out.print("Food added:" + food.displayFoodItem());
+    } catch (IllegalArgumentException illegalArgument) {
+      out.println(illegalArgument.getMessage());
     }
 
-    /**
-     * A function that keeps the program running
-     * Print's out different commands
-     * Switch case to choose
-     */
-    private void run(){
-        boolean running = true;
-        UserinterFase uiFridgeApp  = new UserinterFase();
-        uiFridgeApp.start();
-        while (running){
-            uiFridgeApp.choiceScreen();
-            String chosen = scanner.nextLine();
+  }
 
-            switch(chosen){
-                case "1":
-                    addFoodItem();
-                    break;
-                case "2":
-                    removeFoodItem();
-                    break;
-                case "3":
-                    takeOutItem();
-                    break;
-                case "4":
-                    displayFridge();
-                    break;
-                case "5":
-                    checkExpiredFood();
-                    break;
-                case "6":
-                    running = false;
-                    System.out.println("System ending...");
-                    System.out.println("Goodbye....");
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please try again.");
-            }
-        }
-    }
-
-    /**
-     * Function to add food
-     * Switch case to choose units
-     * IllegalArgumentException argument to catch illegalArgument
-     */
-    private void addFoodItem(){
-        try{
-            out.print("What is the name of the food: ");
-            String nameOfFood = scanner.nextLine();
-
-            out.println("What is the quantity: ");
-            Float amount = scanner.nextFloat();
-            scanner.nextLine();
-
-            UserinterFase uiFridgeApp  = new UserinterFase();
-            uiFridgeApp.choiceOfUnits();
-
-            out.println("Which unit is the food?: ");
-            int unitChosen = scanner.nextInt();
-            scanner.nextLine();
-
-
-            String units;
-            switch (unitChosen){
-                case 1:
-                    units = "kg";
-                    break;
-                case 2:
-                    units = "liter";
-                    break;
-                case 3:
-                    units = "gram";
-                    break;
-                case 4:
-                    units = "stk";
-                    break;
-                default:
-                    out.println("Invalid unit choice. Food item not added.");
-                    return;
-            }
-
-
-            out.println("What is the price per unit of the food (Kilo/KR):");
-            Double price = scanner.nextDouble();
-            scanner.nextLine();
-
-            out.print("Enter expiration date (yyyy-MM-dd): ");
-            String expiration = scanner.nextLine();
-            LocalDate expirationDate = LocalDate.parse(expiration, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-            /**
-             * if(units.equals("gram")){
-             *                 FoodItem.setAmount(FoodItem.getAmount() * 1000);
-             *             }
-             */
-
-
-            FoodItem food = new FoodItem(nameOfFood, amount, units, price, expirationDate);
-            fridge.add(food);
-            out.print("Food added:" + food.displayFoodItem());
-        }
-        catch(IllegalArgumentException illegalArgument){
-            out.println(illegalArgument.getMessage());
-        }
+  /**
+   * Function to remove food Item.
+   * For-each-loop to find the food in the fridge arraylist.
+   */
+  private void removeFoodItem() {
+    out.println("What do you want to remove?: ");
+    String foodToRemove = scanner.nextLine();
+    boolean found = false;
+    Iterator<FoodItem> iteratorObject = fridge.iterator();
+    while (iteratorObject.hasNext()) {
+      FoodItem food = iteratorObject.next();
+      if (food.getNameOfFood().equalsIgnoreCase(foodToRemove)) {
+        iteratorObject.remove();
+        out.println("Removed food: " + foodToRemove);
+        found = true;
+      }
 
     }
-
-    /**
-     * Function to remove food Item.
-     * For-each-loop to find the food in the fridge arraylist.
-     */
-    private void removeFoodItem(){
-        out.println("What do you want to remove?: ");
-        String foodToRemove = scanner.nextLine();
-        boolean found = false;
-        Iterator<FoodItem> iteratorObject = fridge.iterator();
-        while(iteratorObject.hasNext()){
-            FoodItem food = iteratorObject.next();
-            if(food.getNameOfFood().equalsIgnoreCase(foodToRemove)){
-                iteratorObject.remove();
-                out.println("Removed food: " + foodToRemove);
-                found = true;
-            }
-
-        }
-        if(!found){
-            out.println("The item was not found");
-        }
+    if (!found) {
+      out.println("The item was not found");
     }
+  }
 
-    private void takeOutItem(){
-        out.println("What do you want to take out?: ");
-        String foodToTake = scanner.nextLine();
+  private void takeOutItem() {
+    out.println("What do you want to take out?: ");
+    String foodToTake = scanner.nextLine();
 
-        out.println("What is the quantity you want to take out? ");
-        Float amountToTake = scanner.nextFloat();
-        scanner.nextLine();
+    out.println("What is the quantity you want to take out? ");
+    Float amountToTake = scanner.nextFloat();
+    scanner.nextLine();
 
-        for(FoodItem food: fridge){
-            if(food.getNameOfFood().equalsIgnoreCase(foodToTake)){
-                food.setAmount(food.getAmount() - amountToTake);
-                out.println("The food you want to take out is " +  foodToTake + " " + food.getAmount() + food.getUnits());
-            }
-
-        }
-    }
-
-    /**
-     * Function to display food.
-     * If-statement to see if fridge is empty.
-     * Else-statement to go through the whole list.
-     * Calculate the total food price.
-     */
-    private void displayFridge(){
-        out.println("\n--- Fridge Contents ---");
-        if(fridge.isEmpty()){
-            out.println("The fridge is empty.");
-        }
-        else{
-            double totalPrice = 0;
-            for(FoodItem food : fridge){
-                out.println(food.displayFoodItem());
-                totalPrice = totalPrice + food.getAmount() * food.getPrice();
-            }
-            out.println("Total price of all items in the fridge: " + totalPrice + " kr.");
-        }
+    for (FoodItem food : fridge) {
+      if (food.getNameOfFood().equalsIgnoreCase(foodToTake)) {
+        food.setAmount(food.getAmount() - amountToTake);
+        out.println("The food you want to take out is "
+            + foodToTake
+            + " "
+            + food.getAmount()
+            + food.getUnits());
+      }
 
     }
+  }
 
-    /**
-     * Function that checks for expired food.
-     * For-each-loop checks trough whole fridge arraylist.
-     * Calculates and print the name and total price.
-     */
-    private void checkExpiredFood(){
-        out.println("\n--- Expired Food ---");
-        LocalDate currentDate = LocalDate.now();
-        double totalPrice = 0;
-        for(FoodItem food : fridge){
-            if(food.getExpirationDate().isBefore(currentDate)){
-               out.print(food.getNameOfFood() + " has expired.");
-               totalPrice += food.getPrice();
-            }
-            else{
-                out.println("There are no expired food");
-            }
-        }
-        out.println("Total price of expired food is: " + totalPrice + "kr");
+  /**
+   * Function to display food.
+   * If-statement to see if fridge is empty.
+   * Else-statement to go through the whole list.
+   * Calculate the total food price.
+   */
+  private void displayFridge() {
+    out.println("\n--- Fridge Contents ---");
+    if (fridge.isEmpty()) {
+      out.println("The fridge is empty.");
+    } else {
+      double totalPrice = 0;
+      for (FoodItem food : fridge) {
+        out.println(food.displayFoodItem());
+        totalPrice = totalPrice + food.getAmount() * food.getPrice();
+      }
+      out.println("Total price of all items in the fridge: " + totalPrice + " kr.");
     }
 
-    /**
-     * Main function that runs the FridgeApp.
-     * @param args FridgeAPP
-     */
-    public static void main(String[] args) {
-        FridgeApp app = new FridgeApp();
-        app.run();
+  }
+
+  /**
+   * Function that checks for expired food.
+   * For-each-loop checks trough whole fridge arraylist.
+   * Calculates and print the name and total price.
+   */
+  private void checkExpiredFood() {
+    out.println("\n--- Expired Food ---");
+    LocalDate currentDate = LocalDate.now();
+    double totalPrice = 0;
+    for (FoodItem food : fridge) {
+      if (food.getExpirationDate().isBefore(currentDate)) {
+        out.print(food.getNameOfFood() + " has expired.");
+        totalPrice += food.getPrice();
+      } else {
+        out.println("There are no expired food");
+      }
     }
+    out.println("Total price of expired food is: " + totalPrice + "kr");
+  }
+
+  /**
+   * Main function that runs the FridgeApp.
+   *
+   * @param args FridgeAPP
+   */
+  public static void main(String[] args) {
+    FridgeApp app = new FridgeApp();
+    app.run();
+  }
 
 }
--- src/main/java/edu/ntnu/idi/bidata/food/FoodItem.java
+++ src/main/java/edu/ntnu/idi/bidata/food/FoodItem.java
@@ -3,142 +3,146 @@ package edu.ntnu.idi.bidata.food;
 import java.time.LocalDate;
 
 /**
- * Class for FoodItem
+ * Class for <code>FoodItem</code>.
  */
 public class FoodItem {
-    private String nameOfFood; //used food instead
-    private Float amount;
-    private String units;
-    private LocalDate expirationDate;
-    private Double price;
+  private String nameOfFood; //used food instead
+  private Float amount;
+  private String units;
+  private LocalDate expirationDate;
+  private Double price;
 
-    /**
-     * A constructor for the class FoodItem.
-     *
-     * @param nameOfFood     the name of FoodItem.
-     * @param amount         of Grocery FoodItem.
-     * @param units          of the FoodItem.
-     * @param price          of FoodItem.
-     * @param expirationDate of FoodItem.
-     */
-    public FoodItem(String nameOfFood, Float amount, String units, Double price, LocalDate expirationDate) {
-        setNameOfFood(nameOfFood);
-        setAmount(amount);
-        setUnits(units);
-        setPrice(price);
-        setExpirationDate(expirationDate);
-    }
+  /**
+   * A constructor for the class FoodItem.
+   *
+   * @param nameOfFood     the name of FoodItem.
+   * @param amount         of Grocery FoodItem.
+   * @param units          of the FoodItem.
+   * @param price          of FoodItem.
+   * @param expirationDate of FoodItem.
+   */
+  public FoodItem(String nameOfFood, Float amount, String units,
+                  Double price, LocalDate expirationDate) {
+    setNameOfFood(nameOfFood);
+    setAmount(amount);
+    setUnits(units);
+    setPrice(price);
+    setExpirationDate(expirationDate);
+  }
 
-    /**
-     * <code>nameOfFood</code>
-     * @return nameOfFood
-     */
-    public String getNameOfFood() {
-        return nameOfFood;
-    }
+  /** Mutates the <code>nameOfFood</code>.
+   *
+   * @return nameOfFood
+   */
+  public String getNameOfFood() {
+    return nameOfFood;
+  }
 
-    /**
-     * Mutate the <code>nameOfFood</code>
-     * @param nameOfFood the FoodItem's name.
-     * @throws IllegalArgumentException if nameOfFood null, empty or blank.
-     */
-    public void setNameOfFood(String nameOfFood) throws IllegalArgumentException {
-        if (nameOfFood == null || nameOfFood.isEmpty() || nameOfFood.isBlank()) {
-            throw new IllegalArgumentException("No input in name of food");
-        }
-        this.nameOfFood = nameOfFood;
+  /**
+   * Mutate the <code>nameOfFood</code>.
+   *
+   * @param nameOfFood the FoodItem's name.
+   * @throws IllegalArgumentException if nameOfFood null, empty or blank.
+   */
+  public void setNameOfFood(String nameOfFood) throws IllegalArgumentException {
+    if (nameOfFood == null || nameOfFood.isEmpty() || nameOfFood.isBlank()) {
+      throw new IllegalArgumentException("No input in name of food");
     }
+    this.nameOfFood = nameOfFood;
+  }
 
-    /**
-     * Return the amount.
-     *
-     * @return the amount of FoodItem.
-     */
-    public Float getAmount() {
-        return amount;
-    }
+  /**
+   * Return the amount.
+   *
+   * @return the amount of FoodItem.
+   */
+  public Float getAmount() {
+    return amount;
+  }
 
-    /**
-     *
-     * Mutates the <code>amount</code>.
-     * Exception to check that amount is not a negative number.
-     * Throws an IllegalArgumentException if Amount is a negative number.
-     *
-     * @param amount the FoodItem's amount.
-     * @throws IllegalArgumentException if amount is less than 0.1
-     */
-    public void setAmount(Float amount) throws IllegalArgumentException {
-        if (amount < 0.1) {
-            throw new IllegalArgumentException("Amount can not be under 0");
-        }
-        this.amount = amount;
+  /**
+   * Mutates the <code>amount</code>.
+   * Exception to check that amount is not a negative number.
+   * Throws an IllegalArgumentException if Amount is a negative number.
+   *
+   * @param amount the FoodItem's amount.
+   * @throws IllegalArgumentException if amount is less than 0.1
+   */
+  public void setAmount(Float amount) throws IllegalArgumentException {
+    if (amount < 0.1) {
+      throw new IllegalArgumentException("Amount can not be under 0");
     }
+    this.amount = amount;
+  }
 
-    /**
-     * Returns the units.
-     * @return units of the FoodItem.
-     */
-    public String getUnits() {
-        return units;
-    }
+  /**
+   * Returns the units.
+   *
+   * @return units of the FoodItem.
+   */
+  public String getUnits() {
+    return units;
+  }
 
-    /**
-     * Mutates the <code>units</code>.
-     * @param units the FoodItem's unit.
-     * @throws IllegalArgumentException if units is null, empty or blank.
-     */
-    public void setUnits(String units) throws IllegalArgumentException {
-        if (units == null || units.isEmpty() || units.isBlank()) {
-            throw new IllegalArgumentException("No input of units");
-        }
-        this.units = units;
+  /**
+   * Mutates the <code>units</code>.
+   *
+   * @param units the FoodItem's unit.
+   * @throws IllegalArgumentException if units is null, empty or blank.
+   */
+  public void setUnits(String units) throws IllegalArgumentException {
+    if (units == null || units.isEmpty() || units.isBlank()) {
+      throw new IllegalArgumentException("No input of units");
     }
+    this.units = units;
+  }
 
-    /**
-     * Return the expirationDate.
-     *
-     * @return expirationDate of the FoodItem.
-     */
-    public LocalDate getExpirationDate() {
-        return expirationDate;
-    }
+  /**
+   * Return the expirationDate.
+   *
+   * @return expirationDate of the FoodItem.
+   */
+  public LocalDate getExpirationDate() {
+    return expirationDate;
+  }
 
-    /**
-     * Mutates the <code>expirationDate</code>.
-     *
-     * @param expirationDate the FoodItem's expiration date.
-     * @throws IllegalArgumentException if expirationDate is in the past.
-     */
-    public void setExpirationDate(LocalDate expirationDate)throws IllegalArgumentException  {
-        if (expirationDate.isBefore(LocalDate.now())) {
-            throw new IllegalArgumentException("Expiration date cannot be in the past.");
-        }
-        this.expirationDate = expirationDate;
+  /**
+   * Mutates the <code>expirationDate</code>.
+   *
+   * @param expirationDate the FoodItem's expiration date.
+   * @throws IllegalArgumentException if expirationDate is in the past.
+   */
+  public void setExpirationDate(LocalDate expirationDate) throws IllegalArgumentException {
+    if (expirationDate.isBefore(LocalDate.now())) {
+      throw new IllegalArgumentException("Expiration date cannot be in the past.");
     }
+    this.expirationDate = expirationDate;
+  }
 
-    /**
-     * Returns the price.
-     *
-     * @return the price of the FoodItem.
-     */
-    public Double getPrice() {
-        return price;
-    }
+  /**
+   * Returns the price.
+   *
+   * @return the price of the FoodItem.
+   */
+  public Double getPrice() {
+    return price;
+  }
 
-    /**
-     * Mutates the <code>price</code>.
-     * @param price the FoodItem's price.
-     * @throws IllegalArgumentException if price is a negative number.
-     */
-    public void setPrice(Double price)throws IllegalArgumentException  {
-        if (price < 0) {
-            throw new IllegalArgumentException("Price can not be a negative input.");
-        }
-        this.price = price;
+  /**
+   * Mutates the <code>price</code>.
+   *
+   * @param price the FoodItem's price.
+   * @throws IllegalArgumentException if price is a negative number.
+   */
+  public void setPrice(Double price) throws IllegalArgumentException {
+    if (price < 0) {
+      throw new IllegalArgumentException("Price can not be a negative input.");
     }
+    this.price = price;
+  }
 
-    public String displayFoodItem() {
-        return String.format("%s - %.1f %s, Price: %.2f, Expiration Date: %s",
-                nameOfFood, amount, units, price*amount, expirationDate);
-    }
+  public String displayFoodItem() {
+    return String.format("%s - %.1f %s, Price: %.2f, Expiration Date: %s",
+        nameOfFood, amount, units, price * amount, expirationDate);
+  }
 }
