8824fe2bb9d85f4df5d33e7e768f8e1dfaf89076
arpit <arpitsa@ntnu.no>
2024-11-20 17:05:44 +0100

    made a more robust kode for getValidPrice, getValidAmount and added getValidExpirationDate

--- src/main/java/edu/ntnu/idi/bidata/InputReader.java
+++ src/main/java/edu/ntnu/idi/bidata/InputReader.java
@@ -1,13 +1,18 @@
 package edu.ntnu.idi.bidata;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 import static java.lang.System.in;
 
 public class InputReader {
   private final Scanner scanner;
+  private final UIPrintHandler print;
+
 
 
   public InputReader() {
     scanner = new Scanner(in);
+    print = new UIPrintHandler();
   }
 
   public String scannerString(){
@@ -15,21 +20,60 @@ public class InputReader {
     return nameOfFood.toLowerCase();
   }
 
-  public Double scannerPriceOfFood(){
-    Double price = scanner.nextDouble();
-    scanner.nextLine();
+  public Double getValidPrice(){
+    double price = 0.0;
+    boolean validPrice = false;
+    while (!validPrice) {
+      print.pricePerUnitOutput();
+      String priceInput = scannerString();
+      price = Double.parseDouble(priceInput);
+      if (price < 0) {
+        print.invalidPriceOutput();
+      } else {
+        validPrice = true;
+      }
+    }
     return price;
   }
 
-  public Float scannerAmountOfFood(){
-     Float amount = scanner.nextFloat();
-     scanner.nextLine();
-     return amount;
+  /**
+   * Prompts the user to enter an expiration date and validates its format.
+   *
+   * <p>This method continually prompts the user to enter a date until a valid
+   * expiration date is provided in the format yyyy-MM-dd. If the date format is invalid,
+   * an error message is displayed, and the user is prompted to try again.
+   * </p>
+   *
+   * @return a valid {@link LocalDate} object representing the expiration date
+   */
+  public LocalDate getValidExpirationDate() {
+    LocalDate expirationDate = null; // (OpenAI, 2024)
+    boolean validDate = false;
+    while (!validDate) {
+      print.expirationDateOutput();
+      String expiration = scannerString();
+      try {
+        expirationDate = LocalDate.parse(expiration, DateTimeFormatter.ofPattern("yyyy-MM-dd")); // (OpenAI, 2024)
+        validDate = true;
+      } catch (Exception e) {
+        print.invalidExpirationDateOutput();
+      }
+    }
+    return expirationDate;
   }
 
-
-
-
-
-
+  public Float getValidAmount(){
+    float amount = 0f;
+    boolean validAmount = false;
+    while (!validAmount) {
+      String priceInput = scannerString();
+      try {
+        amount = Float.parseFloat(priceInput);
+        validAmount = true;
+      } catch (Exception e){
+        print.invalidFoodAmountOutput();
+      }
+    }
+    return amount;
+  }
 }
