5b6c5f682406cdf8381e5d0d47adbfdb1ee953cb
arpit <arpitsa@ntnu.no>
2024-11-21 14:50:29 +0100

    Made a new test class FridgeStorageTest.

---
+++ src/test/java/edu/ntnu/idi/bidata/fridge/FridgeStorageTest.java
@@ -0,0 +1,42 @@
+package edu.ntnu.idi.bidata.fridge;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FridgeStorageTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addFoodItem() {
+    }
+
+    @Test
+    void removeFoodItem() {
+    }
+
+    @Test
+    void foodToTake() {
+    }
+
+    @Test
+    void searchFoodByName() {
+    }
+
+    @Test
+    void getIterator() {
+    }
+
+    @Test
+    void getIteratorAlphabetical() {
+    }
+}
