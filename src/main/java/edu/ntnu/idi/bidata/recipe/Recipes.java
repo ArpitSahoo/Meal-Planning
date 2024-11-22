396bc0a5bd5966a9f353ee4772f59d3a2aad7572
arpit <arpitsa@ntnu.no>
2024-11-22 22:38:22 +0100

    Made a new class called Recipes.

---
+++ src/main/java/edu/ntnu/idi/bidata/recipe/Recipes.java
@@ -0,0 +1,47 @@
+package edu.ntnu.idi.bidata.recipe;
+
+public class Recipes {
+    private String nameOfRecipe;
+    private String description;
+    private String steps;
+    private String ingredients;
+
+    public Recipes(String nameOfRecipe, String description, String steps, String ingredients) {
+        setNameOfRecipe(nameOfRecipe);
+        setDescription(description);
+        setSteps(steps);
+        setIngredients(ingredients);
+    }
+
+    public String getNameOfRecipe() {
+        return nameOfRecipe;
+    }
+
+    public void setNameOfRecipe(String nameOfRecipe) {
+        this.nameOfRecipe = nameOfRecipe;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSteps() {
+        return steps;
+    }
+
+    public void setSteps(String steps) {
+        this.steps = steps;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+}
