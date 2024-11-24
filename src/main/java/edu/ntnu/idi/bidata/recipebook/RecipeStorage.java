81793f2c427d2d20de46cd56d6b69f5175866a65
arpit <arpitsa@ntnu.no>
2024-11-24 18:01:15 +0100

    RecipeStorage.java v0.0.2 Made an addRecipe(), getRecipe(), addIngredientForRecipe() method and imported utils and FoodItem class.
    Imported utils, Recipes class and added documentation.

--- src/main/java/edu/ntnu/idi/bidata/recipebook/RecipeStorage.java
+++ src/main/java/edu/ntnu/idi/bidata/recipebook/RecipeStorage.java
@@ -1,18 +1,45 @@
 package edu.ntnu.idi.bidata.recipebook;
-/**
- * @version 0.0.1
- */
 
 import edu.ntnu.idi.bidata.recipe.Recipes;
-
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * represents the Recipe book.
+ * @version 0.0.2
+ */
 public class RecipeStorage {
     private final Map<String, Recipes> recipesMap;
 
     public RecipeStorage() {
         recipesMap = new HashMap<>();
     }
+
+    public boolean addRecipe(Recipes newRecipe) {
+        boolean wasAdded;
+        if (recipesMap.containsKey(newRecipe.getNameOfRecipe())) {
+            wasAdded = false;
+        } else {
+            recipesMap.put(newRecipe.getNameOfRecipe(), newRecipe);
+            wasAdded = true;
+        }
+        return wasAdded;
+    }
+
+    public Recipes getRecipe (String recipeName) {
+        return recipesMap.get(recipeName);
+    }
+
+    public boolean addIngredientForRecipe(String nameOfRecipe, String name, Float amount, String unit) {
+        boolean exists = false;
+        Recipes exRecipe = recipesMap.get(nameOfRecipe);
+        if(exRecipe != null){
+            exRecipe.addIngredient(name, amount, unit);
+            exists = true;
+        }
+        return exists;
+    }
+
+
 
 }
