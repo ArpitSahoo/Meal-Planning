80f7a66d0f9fae13de90659e653ee14def46b418
arpit <arpitsa@ntnu.no>
2024-11-24 16:08:34 +0100

    FridgeApp v0.0.1
    Updated the documentation.

--- src/main/java/edu/ntnu/idi/bidata/FridgeApp.java
+++ src/main/java/edu/ntnu/idi/bidata/FridgeApp.java
@@ -14,13 +14,12 @@ public class FridgeApp {
   /**
    * The main entry point for the fridge management application.
    *
-   * <p>This method creates an instance of the {@link UserInterface} class and starts the user interface.
+   * <p>This method creates an instance of the {@link UserInterface} class and starts the user interface(UI).
    * After starting the UI, it prints the fridge contents. If any {@link IllegalArgumentException}
    * is thrown, the error message is displayed.</p>
    *
    * @param args command-line arguments (not used in this method)
    */
-  //TODO update this
   public static void main(String[] args){
     try{
       new UserInterface();
