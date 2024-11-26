7ad0c302edc01e14a19ceee1d0a8a6e7c077c33e
arpit <arpitsa@ntnu.no>
2024-11-26 11:23:45 +0100

    RecipeStorage.java v0.0.3
    Fixed the indentation to fit the Google check style standard.

--- src/main/java/edu/ntnu/idi/bidata/recipebook/RecipeStorage.java
+++ src/main/java/edu/ntnu/idi/bidata/recipebook/RecipeStorage.java
@@ -12,54 +12,54 @@ import java.util.Map;
  * This class allows the user add, remove and get the name of the recipe.</p>
  *
  * @author Arpit Sahoo
- * @version 0.0.2
+ * @version 0.0.3
  * @since 0.0.1
  */
 public class RecipeStorage {
-    private final Map<String, Recipes> recipesMap;
+  private final Map<String, Recipes> recipesMap;
 
-    /**
-     * Constructs a {@code RecipeStorage} instance and initializes the recipes Map.
-     *
-     * <p>This constructor initializes the {@code recipesMap} as a new {@link HashMap}.
-     * </p>
-     */
-    public RecipeStorage() {
-        recipesMap = new HashMap<>();
-    }
+  /**
+   * Constructs a {@code RecipeStorage} instance and initializes the recipes Map.
+   *
+   * <p>This constructor initializes the {@code recipesMap} as a new {@link HashMap}.
+   * </p>
+   */
+  public RecipeStorage() {
+    recipesMap = new HashMap<>();
+  }
 
-    /**
-     * Adds a recipe to the recipe book.
-     *
-     * <p>If the recipe is already in the recipe book (based on its name)
-     * the method does not add the recipe.
-     * If the food item is not found in the recipe book,
-     * the recipe will be added as a new entry.</p>
-     *
-     * @param newRecipe the {@link Recipes} to be added or updated in the fridge
-     * @return {@code true} if the recipe was added.
-     * {@code false} if the recipe was not added.
-     */
-    public boolean addRecipe(Recipes newRecipe) {
-        boolean wasAdded;
-        if (recipesMap.containsKey(newRecipe.getNameOfRecipe())) {
-            wasAdded = false;
-        } else {
-            recipesMap.put(newRecipe.getNameOfRecipe(), newRecipe);
-            wasAdded = true;
-        }
-        return wasAdded;
+  /**
+   * Adds a recipe to the recipe book.
+   *
+   * <p>If the recipe is already in the recipe book (based on its name)
+   * the method does not add the recipe.
+   * If the food item is not found in the recipe book,
+   * the recipe will be added as a new entry.</p>
+   *
+   * @param newRecipe the {@link Recipes} to be added or updated in the fridge
+   * @return {@code true} if the recipe was added.
+   *{@code false} if the recipe was not added.
+   */
+  public boolean addRecipe(Recipes newRecipe) {
+    boolean wasAdded;
+    if (recipesMap.containsKey(newRecipe.getNameOfRecipe())) {
+      wasAdded = false;
+    } else {
+      recipesMap.put(newRecipe.getNameOfRecipe(), newRecipe);
+      wasAdded = true;
     }
+    return wasAdded;
+  }
 
-    /**
-     * Gets the name of the recipe from the recipe book.
-     *
-     * @param recipeName the name of the recipe.
-     * @return the name of the recipe.
-     */
-    public Recipes getRecipe(String recipeName) {
-        return recipesMap.get(recipeName); // allows the other method get the name of the recipe
-    }
+  /**
+   * Gets the name of the recipe from the recipe book.
+   *
+   * @param recipeName the name of the recipe.
+   * @return the name of the recipe.
+   */
+  public Recipes getRecipe(String recipeName) {
+    return recipesMap.get(recipeName); // allows the other method get the name of the recipe
+  }
 
 
 }
